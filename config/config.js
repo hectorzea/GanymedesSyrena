d70beebc9fdbf935fbcc1c6730a677e9ce61aa09
Hector <hzea@inclusionservices.com>
2020-09-30 22:43:00 -0300

    d

--- config/config.js
+++ config/config.js
@@ -7,4 +7,4 @@ process.env.PORT = process.env.PORT || 3001;
 //MONGODB CONNECTION
 let DatabaseURL;
 DatabaseURL = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/sirena_db' : process.env.remoteURL
-process.env.DATABASEURL = "mongodb+srv://hectorzea:dtW3YP1cnf0f2Fpe@cluster0.7xou3.mongodb.net/sirena_db";
+process.env.DATABASEURL = DatabaseURL;
